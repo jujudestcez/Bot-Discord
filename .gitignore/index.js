8e75186aa984ba601d1a213c4fa4b55686503cc1
jujudestcez <43293222+jujudestcez@users.noreply.github.com>
2018-09-22 18:34:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -65,4 +65,25 @@ bot.on('message', message => {
         .addField("Utilisateurs sur le Discord", message.guild.memberCount)
     message.channel.sendEmbed(embed)
     }
+    
+    if (message.content.startsWith(prefix + "sondage")) {
+        if(message.author.id == "259002735301492736"){
+            let args = message.content.split(" ").slice(1);
+            let thingToEcho = args.join(" ")
+            var embed = new Discord.RichEmbed()
+                .setDescription("Sondage")
+                .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
+                .setColor("0xB40404")
+                .setTimestamp()
+            message.guild.channels.find("name", "sondage").sendEmbed(embed)
+            .then(function (message) {
+                message.react("✔")
+                message.react("✖")
+            }).catch(function() {
+            });
+            }else{
+                return message.reply("Tu n'as pas la permission.")
+            }}
+        }
+    }
 });
