835cfed1866ed6ca6699cc6717ea488aa71d8e0e
jujudestcez <43293222+jujudestcez@users.noreply.github.com>
2018-09-23 14:19:57 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -78,7 +78,7 @@ bot.on('message', message => {
             message.guild.channels.find("name", "sondage").sendEmbed(embed)
             .then(function (message) {
                 message.react("1️⃣")
-                message.react("")
+                message.react("1️⃣")
             }).catch(function() {
             });
     }}
