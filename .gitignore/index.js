4a02acb134f9096c8292610016a6441c58374c15
jujudestcez <43293222+jujudestcez@users.noreply.github.com>
2018-09-23 13:14:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -69,9 +69,9 @@ bot.on('message', message => {
     if (message.content.startsWith(prefix + "sondage")) {
         if(message.author.id == "259002735301492736"){
             let args = message.content.split(" ").slice(1);
-            let thingToEcho = args.join("cc ")
+            let thingToEcho = args.join(" ")
             var embed = new Discord.RichEmbed()
-                .setDescription("coucou")
+                .setDescription("Sondage")
                 .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
                 .setColor("#2ecc71")
                 .setTimestamp()
