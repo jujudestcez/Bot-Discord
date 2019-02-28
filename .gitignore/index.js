01f8b469eedafa77428476048130d62cec06297d
jujudestcez <43293222+jujudestcez@users.noreply.github.com>
2019-02-28 20:59:45 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -17,8 +17,8 @@ bot.login(process.env.TOKEN); /*token scumbot*/
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){ /*!help*/
-        message.channel.sendMessage(":clipboard: | **Liste des commands**");
-        message.channel.sendMessage("*!regles* : les règles du serveur")
+        message.autor.send(":clipboard: | **Liste des commands**");
+        message.autor.send("*!regles* : les règles du serveur")
     }
 
     if (message.content === prefix + "bmscum"){
